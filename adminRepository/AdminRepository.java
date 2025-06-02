079f130650391199c1153cd3294a525c0151c1b5
Sayari Das <sayaridas04@gmail.com>
2025-06-02 10:58:20 +0530

    Delete adminRepository directory

--- adminRepository/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package Admin.admin.adminRepository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import Admin.admin.adminModel.*;
-
-@Repository
-public interface AdminRepository extends JpaRepository<AdminModel, Integer> {
-}
